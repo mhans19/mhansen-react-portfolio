55b4e04a3e54a2f91cbbe07e7b3b0691553eac08
mhans19 <mdhansen19@gmail.com>
2020-09-14 23:13:27 -0600

    aesthetics for projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 function Projects(props) {
   return (
     <div id="projects">
+      <h1 className = "header text-center">Projects</h1>
         <div class="container my-4">
             <div class="row">
                 {props.proj.map(item => (
                     <Card style={{ width: '22rem', margin: '10px', padding:'10px' }}>
                       <Card.Img variant="top" src={require(`../../assets/images/${item.img}`)}/>
-                      <Card.Body>
+                      <Card.Body className = "text-center">
                           <Card.Title>{item.name}</Card.Title>
-                      </Card.Body>
-                      <Card.Body>
-                          <Card.Link href={item.url}>GitHub Repository </Card.Link>
-                          <Card.Link href={item.url}>Another Link</Card.Link>
+                          <Button className = "mx-1" href={item.url} variant="secondary">Repository</Button>
+                          <Button href={item.url} variant="secondary">Application</Button>
                       </Card.Body>
                     </Card>
                 ))}
